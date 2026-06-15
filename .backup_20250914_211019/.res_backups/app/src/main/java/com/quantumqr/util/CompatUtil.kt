0681fbb068e5d2a27dda1367a0b2cbf8b1c88a75
brandonmarkgaia-hub <brandonmarkgaia@gmail.com>
2026-06-15 08:03:39 +0200

    Remove committed backups and scratch files
    Remove committed backups and scratch files

--- .backup_20250914_211019/.res_backups/app/src/main/java/com/quantumqr/util/CompatUtil.kt
+++
@@ -1,9 +0,0 @@
-package com.quantumqr.util
-
-object CompatUtil {
-    fun isUrl(text: String): Boolean = try {
-        android.util.Patterns.WEB_URL.matcher(text).matches()
-    } catch (_: Throwable) {
-        Regex("^(https?://|www\\.)", RegexOption.IGNORE_CASE).containsMatchIn(text)
-    }
-}
